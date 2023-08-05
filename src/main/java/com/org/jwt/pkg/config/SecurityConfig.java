ad2383e5b2fc998838d9212c2673381ecc62c19c
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-08-05 18:32:22 +0530

    spring 3 and jwt integration

---
+++ src/main/java/com/org/jwt/pkg/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.org.jwt.pkg.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.org.jwt.pkg.security.JWTAuthenticationFilter;
+import com.org.jwt.pkg.security.JwtAuthenticationEntryPoint;
+
+@Configuration
+public class SecurityConfig {
+	
+	    @Autowired
+	    private JwtAuthenticationEntryPoint point;
+	    
+	    @Autowired
+	    private JWTAuthenticationFilter filter;
+	    
+	    @Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+	        http.csrf(csrf -> csrf.disable())
+	                .authorizeRequests().
+	                requestMatchers("/test").authenticated().requestMatchers("/login").permitAll()
+	                .anyRequest()
+	                .authenticated()
+	                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+	                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+	        return http.build();
+	    }
+}
---
+++ src/main/java/com/org/jwt/pkg/model/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.org.jwt.pkg.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+	private String username;
+	private String jwtToken;
+}
