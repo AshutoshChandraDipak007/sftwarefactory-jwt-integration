f7dd04f358374da7080b1353b99287bec2216363
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-08-06 14:12:50 +0530

    added documentation link

--- src/main/java/com/org/jwt/pkg/Springboot3JwtIntegrationApplication.java
+++ src/main/java/com/org/jwt/pkg/Springboot3JwtIntegrationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot3JwtIntegrationApplication {
 
+	//https://www.learncodewithdurgesh.com/blogs/jwt-authentication-with-spring-boot-31
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3JwtIntegrationApplication.class, args);
 	}
